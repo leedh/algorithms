1df799198dab6cb7ecb5eb516e4e8c244fa872f9
leedh <leedonghee@protonmail.com>
2023-10-30 15:22:54 +0900

    Update 125_Valid_Palindrome.py

--- leetcode/125_Valid_Palindrome.py
+++ leetcode/125_Valid_Palindrome.py
@@ -2,13 +2,12 @@ import string
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-            s = s.lower()
-                    s = s.replace(" " , "")
-                            for character in string.punctuation:
-                                        s = s.replace(character, "")
-
-                                                        s_reverse = s[::-1]
-                                                                if s == s_reverse:
-                                                                            return True
-                                                                                    else:
-                                                                                                return False
+        s = s.lower()
+        s = s.replace(" " , "")
+        for character in string.punctuation:
+            s = s.replace(character, "")
+        s_reverse = s[::-1]
+        if s == s_reverse:
+            return True
+        else:
+            return False
